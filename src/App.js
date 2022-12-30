22c2e6e2c7b34b6975462971a972c3a9d8ce8fb8
Nicat <tu72k7cwp@code.edu.az>
2022-12-31 02:40:05 +0400

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [data,setData] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <Formik
+        initialValues={{
+          name:"",
+          email: "",
+          lastname:"",
+          about:"",
+          phonenumber:"",
+        }}
+        onSubmit={(values)=>{
+          setData(prevState=>[...prevState,values])
+          console.log(values);
+        }} 
         >
-          Learn React
-        </a>
-      </header>
+        {(props)=>(
+          <Form className='inplar'>
+            <Field className="inp" name="name" placeholder="Name..."/>
+            <Field className="inp" name="lastname"placeholder="Last Name..."/>
+            <Field className="inp" name="email" type="email" placeholder="Enter e-mail adres..."/>
+            <Field className="inp" name="phonenumber" type="number" placeholder="Phone Number..."/>
+            <Field className="inp" name="about" type="text" placeholder="About..."/>
+            <button type='submit'>Submit</button>
+            </Form>
+        )} 
+      </Formik>
+      {
+        <ul>
+          {data.map((element,index)=>{
+            return (
+              <li key={index}>
+                <p>Name: {element.name}</p>
+                <p>Last Name: {element.lastname}</p>
+                <p>E-mail: {element.email}</p>
+                <p>Phone Number: +994 {element.phonenumber}</p>
+                <p>About: {element.about}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
     </div>
   );
 }
